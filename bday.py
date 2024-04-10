86298b41007605c6572468118da75a9a2bacd4e6
Zack <51299535+zacharysnewman@users.noreply.github.com>
2024-04-10 10:26:31 -0700

    Update bday.py

--- bday.py
+++ bday.py
@@ -2,6 +2,8 @@
 
 import curses
 import random
+import base64
+import bz2
 
 stdscr = curses.initscr()
 curses.noecho()
@@ -10,7 +12,7 @@ stdscr.keypad(1)
 curses.halfdelay(1)
 
 
-bg_frame = 'QlpoOTFBWSZTWbQ7eg8AAMr7gH/doChY84AQAATAAAAFMAF5QmDQEaEj00I0AyETaJqA9RoAGgCQkRoQjJoYEycRcmNn8JgkdWl1b9yeX1arHCrZQdpdJIGxJB6aEgzYB7ruc8VmVYsQ9WxWrvWPhlxxEqvBsyvw4iTxZwwNiD5QrmDIxRGKLWGUbmaaXxlps9c8t402JqzLay7FleFPkDSzMQg1thxMocVSOoYGvY3DYqWlms3MrxqgwBwjcJ0rQdy4RfmROTTYlz1Y8fJQ07dGkQ4F/F5dtrSQRxtXR3e4t3o338YtLd9vNYnGs754BlgKMk+bGH9JuYnrtylI7gS0NTGA0IyZmt+u6oiSqmdA1mEhp5IqGAYExHK0HIQaAsXtcyUig5VsziVGAHrbKalM/riEKj+SlgWsVJAy5oYEzxjJDNHH4u5IpwoSFodvQeA='.decode('base64').decode('bz2')
+bg_frame = bz2.decompress(base64.b64decode('QlpoOTFBWSZTWbQ7eg8AAMr7gH/doChY84AQAATAAAAFMAF5QmDQEaEj00I0AyETaJqA9RoAGgCQkRoQjJoYEycRcmNn8JgkdWl1b9yeX1arHCrZQdpdJIGxJB6aEgzYB7ruc8VmVYsQ9WxWrvWPhlxxEqvBsyvw4iTxZwwNiD5QrmDIxRGKLWGUbmaaXxlps9c8t402JqzLay7FleFPkDSzMQg1thxMocVSOoYGvY3DYqWlms3MrxqgwBwjcJ0rQdy4RfmROTTYlz1Y8fJQ07dGkQ4F/F5dtrSQRxtXR3e4t3o338YtLd9vNYnGs754BlgKMk+bGH9JuYnrtylI7gS0NTGA0IyZmt+u6oiSqmdA1mEhp5IqGAYExHK0HIQaAsXtcyUig5VsziVGAHrbKalM/riEKj+SlgWsVJAy5oYEzxjJDNHH4u5IpwoSFodvQeA=')).decode("utf-8")
 main_dec = []
 minor_decs = []
 for y, bg_line in enumerate(bg_frame.split('\n')):
@@ -19,9 +21,9 @@ for y, bg_line in enumerate(bg_frame.split('\n')):
       minor_decs.append((y, x, bg_char))
     if y > 7: continue
     if bg_char in '()':
-      if bg_char is '(' and bg_line[x+2] is not ')':
+      if bg_char == '(' and bg_line[x+2] != ')':
         main_dec.append((y, x, bg_char))
-      if bg_char is ')' and bg_line[x-2] is not '(':
+      if bg_char == ')' and bg_line[x-2] != '(':
         main_dec.append((y, x, bg_char))
 
 stdscr.addstr(0, 0, bg_frame)
@@ -29,18 +31,18 @@ stdscr.refresh()
 denominator = 3
 finalized = False
 keyin = stdscr.getch()
-while keyin is -1 or not finalized:
-  if keyin is not -1:
+while keyin == -1 or not finalized:
+  if keyin != -1:
     finalized = True
   random.shuffle(main_dec)
   random.shuffle(minor_decs)
-  for index, dec in enumerate(main_dec[:len(main_dec)/denominator]):
+  for index, dec in enumerate(main_dec[:len(main_dec)//denominator]):
     if not finalized:
       main_dec[index] = (dec[0], dec[1], '()'.strip(dec[2]))
     else:
       main_dec[index] = (dec[0], dec[1], ',')
     stdscr.addstr(dec[0], dec[1], main_dec[index][2])
-  for index, minor_dec in enumerate(minor_decs[:len(minor_decs)/denominator]):
+  for index, minor_dec in enumerate(minor_decs[:len(minor_decs)//denominator]):
     minor_decs[index] = (minor_dec[0], minor_dec[1], '*~'.strip(minor_dec[2]))
     stdscr.addstr(minor_dec[0], minor_dec[1], minor_decs[index][2])
   stdscr.move(0, 0)
